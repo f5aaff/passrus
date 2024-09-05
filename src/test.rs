c00723cada3c636fa875f4cb0bbba04711f27e66
james.dunsmore <james.dunsmore@baesystems.com>
2024-09-05 17:05:08 +0100

    server

--- src/test.rs
+++ src/test.rs
@@ -1,135 +1,8 @@
-use log::warn;
-use rand::{rngs::OsRng, RngCore};
-use std::process::exit;
-mod cryptman;
-mod passman;
-use anyhow::{anyhow, Context, Result};
-use std::fs::{self, File};
-use std::io::{self, Read, Write};
-use std::os::unix::net::{UnixListener, UnixStream};
 
-fn handle_client(mut stream: UnixStream) {
-    let mut buffer = [0; 1024]; // Buffer to store incoming data
-    match stream.read(&mut buffer) {
-        Ok(n) if n > 0 => {
-            let received_message = String::from_utf8_lossy(&buffer[..n]);
-            println!("Received: {}", received_message);
 
-            // Echo back the message
-            stream
-                .write_all(&buffer[..n])
-                .expect("Failed to send response");
-        }
-        _ => {
-            eprintln!("Failed to read from the client");
-        }
-    }
-}
-fn main() -> std::io::Result<()> {
-    let socket_path = "/tmp/rust_echo_service.sock";
-    let store_path = "/tmp/store";
-
-    // Remove any existing socket file
-    if fs::metadata(socket_path).is_ok() {
-        fs::remove_file(socket_path)?;
-    }
-
-    // Create a new Unix domain socket listener
-    let listener = UnixListener::bind(socket_path)?;
-
-    println!("Echo service is running on {}", socket_path);
-
-    // Accept incoming connections in a loop
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => handle_client(stream),
-            Err(err) => eprintln!("Connection failed: {}", err),
-        }
-    }
-
-    Ok(())
-}
-
-// given a string path to the store, and a string of the password, open a passman
-// store. returns the decrypted and instantiated container struct.
-fn open_store(store: String, pass: String) -> std::io::Result<passman::Container> {
-    let mut file = File::open(store)?;
-    let mut buf = Vec::new();
-
-    file.read_to_end(&mut buf)?;
-    // decrypt the content, reading it from file.
-    let dec_res = match cryptman::decrypt_file_mem_gen_key(buf, "", &pass) {
-        Ok(res) => {
-            println!("grabbed salt&nonce from file, decrypted successfully");
-            res
-        }
-
-        Err(error) => {
-            warn!(target:"main","error decrypting data: {error:?}");
-            exit(0);
-        }
-    };
-
-    let mut passes: passman::Container = passman::Container::new("");
-    passes.from_json_arr(dec_res.as_slice())?;
-    Ok(passes)
-}
-
-// given a passman container, a destination string, and a password with which to encrypt it,
-// encrypt and write to file the container to file.
-fn close_store(
-    mut store: passman::Container,
-    dest: String,
-    pass: String,
-) -> Result<(), anyhow::Error> {
-    let key_n_salt = cryptman::pass_2_key(&pass, [0u8; 32])
-        .map_err(|e| anyhow!("error generating key and salt: {:?}", e))?;
-
-    let key = key_n_salt.0;
-    let salt = key_n_salt.1;
-
-    let binding = store.to_json_string();
-    let json_arr = binding.as_bytes();
-
-    // generate a nonce to use, fill with random bytes with OsRng.
-    let mut nonce = [0u8; 24];
-    OsRng.fill_bytes(&mut nonce);
-
-    // encrypt the file
-    let enc_res = cryptman::encrypt_file_mem_with_salt(json_arr.to_vec(), "", &key, &nonce, &salt)?;
-
-    let mut file = File::create(dest)?;
-    file.write_all(enc_res.as_slice())?;
-
-    Ok(())
-}
-
-fn get_entries_by_field(
-    store: passman::Container,
-    target_field: String,
-    target_value: String,
-    password: String,
-) -> Result<Vec<passman::Entry>,anyhow::Error> {
-    let matching_entries = passman::get_entries_by_field(&store, &target_field, &target_value);
-    for mut entry in matching_entries {
-        let vec = &entry.pass_vec;
-        let vec = &vec.clone();
-
-        let lossy_encrypted = String::from_utf8_lossy(vec.as_slice());
-        entry.pass_vec = cryptman::decrypt_file_mem_gen_key(entry.pass_vec, "", &password).unwrap();
-
-        let password = String::from_utf8_lossy(entry.pass_vec.as_slice());
-        println!(
-            "Username: {}\t encrypted:{}\t pass:{}",
-            entry.username, lossy_encrypted, password
-        );
-    }
-    Ok(matching_entries)
-}
 
 #[cfg(test)]
 mod tests {
-
     use crate::cryptman;
     use crate::passman;
     use log::warn;
@@ -137,7 +10,7 @@ mod tests {
     use std::process::exit;
 
     #[test]
-    fn test() {
+    fn full_test() {
         // obligatory garbage password
         let pass = "password";
 
